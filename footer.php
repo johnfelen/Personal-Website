4d8aed28b8f929837f5524c3bc54900c89f75835
jtf28 <jtf28@pitt.edu>
2016-03-09 16:18:22 -0500

    fixed the spacing error with the footer and added icons for github and linkedin there

--- footer.php
+++ footer.php
@@ -1,9 +1,13 @@
-		<div class="header-footer font-vollkorn font-small">
+		<div class="header-footer font-vollkorn">
 			<hr class="brown">
-			<p class="footer-left">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;John Felen's Personal Website</p> 
-			<p class="footer-right">Icons provided by glyphicons.com&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
-			<br><hr class="brown">
+			<p class="footer-left font-small">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;John Felen's Personal Website</p> 
+			<p class="footer-right font-small">
+                <a href="https://github.com/johnfelen" title="Check out my Github"><i class="fa fa-github fa-2x"></i></a>
+                 &nbsp;&nbsp;&nbsp;
+                <a href="https://www.linkedin.com/in/johnfelen" title="Connect with Me"><i class="fa fa-linkedin fa-2x"></i></a>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            </p>
+            <hr class="brown"><hr class="brown"><hr class="brown">  <!--this somehow fixes the way too much space between two <br>s and one <hr>, margins/padding did not fix the error but tripple <hr> works-->
 		</div>
-		
 	</body>
 </html> 
