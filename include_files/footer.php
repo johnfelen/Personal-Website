d0880f7016f72090d00158bf25e080791078fd29
jtf28 <jtf28@pitt.edu>
2016-03-10 14:53:19 -0500

    added resume link to the footer

--- include_files/footer.php
+++ include_files/footer.php
@@ -8,6 +8,9 @@
             <hr class="brown">
             <p class="footer-left font-small">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;John Felen's Personal Website</p> 
             <p class="footer-right font-small">
+            <!--the résumé is under include_files because I did not see a point to make another folder for files when there is only going to be one external file, so I just put it with the php include files-->
+            <a href="../include_files/JohnFelenRésumé.pdf" title="Download my R&eacute;sum&eacute;"><i class="fa fa-file-text fa-2x"></i></a>
+                 &nbsp;&nbsp;&nbsp;
                 <a href="https://github.com/johnfelen" title="Check out my Github"><i class="fa fa-github fa-2x"></i></a>
                  &nbsp;&nbsp;&nbsp;
                 <a href="https://www.linkedin.com/in/johnfelen" title="Connect with Me"><i class="fa fa-linkedin fa-2x"></i></a>
