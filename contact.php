798c5453554a49748f26e15f0e6e4f2ac2b5a62b
jtf28 <jtf28@pitt.edu>
2016-03-10 14:32:31 -0500

    added my bio to the contact me page

--- contact.php
+++ contact.php
@@ -2,8 +2,33 @@
     $pageName = "Contact Me";
     $glyphiconName = "envelope";
     include( "include_files/header.php" );
+    include( "include_files/start-row-10.php" );
 ?>
 
+<p class="font-vollkorn font-medium font-center brown" >
+    I was born at a young age of zero months in Pittsburgh, Pennsylvania. &nbsp;
+    I am now 247 months old. &nbsp;
+    In my trek for an education I went to the school district known as Mt. Lebanon. &nbsp;
+    As a young lad, I  enjoyed learning, thinking while walking aimlessly, challenging myself, and technology. &nbsp;
+    Growing up, I was always a chubby kid probably because when I was first introduced to the internet, I was hooked and a user for life. &nbsp;
+    Well, I have not lived all of my life yet so I can not be so sure. &nbsp;
+    Let's skip a couple chapters to when I lost a good fifty pounds the summer after junior year of high school. &nbsp; 
+    Oddly enough, I did not decide to become a computer science major until I was about to start my senior year. &nbsp;
+    My family and friends in school would usually come to me with their technology woes. &nbsp;
+    Throughout high school I spent many hours helping others in the community. &nbsp;
+    I truly believe that giving is better than receiving, and have centered my, albeit short, career around helping others. &nbsp;
+    Plus, my love for learning, yes I was the kid that did not like it when school was cancelled, allowed me to get through college in three years and along the way become a Peer Tutor and Undergraduate Teaching Assistant. &nbsp;
+    I am interested in a plethora of computer science areas ranging from software engineering and web development to mobile applications and security. &nbsp;
+    I am planning to graduate from the University of Pittsburgh in Spring 2016 and am currently looking for a job post-graduation.
+</p>
+
+<br>
+<p class="font-vollkorn font-medium font-center brown" >
+    If you wish to contact me, please do so through my e-mail address: johnfelen@pitt.edu.  &nbsp;
+    A link to my r&eacute;sum&eacute; is in the footer. &nbsp;
+</p>
+
 <?php
+    include( "include_files/end-row-10.php" );
     include( "include_files/footer.php" );
 ?>
