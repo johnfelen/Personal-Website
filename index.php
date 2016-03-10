5625fd19ec452ce1540b7f09429430bccef441f0
jtf28 <jtf28@pitt.edu>
2016-03-10 17:49:17 -0500

    fixed up a couple of bugs with the blinking and typing of the home page

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <div class="col-xs-3"></div>
         <div class="col-xs-6">
             <img src="../images/me.jpg" class="picture" alt="Picture of Me" width="100%"/>
-            <blink>
+            <blink class="invisible">
                 <p class="font-ubuntu-mono font-medium font-center brown">
                     Click to Continue
                 </p>
@@ -27,18 +27,51 @@
 
 <!--based on second last response on http://stackoverflow.com/questions/18105152/alternative-for-blink since the actual <blink> tag is deprecated-->
 <script type="text/javascript">
-    function blink() {
-    var blinks = document.getElementsByTagName('blink');
-    for (var i = blinks.length - 1; i >= 0; i--) {
-    var s = blinks[i];
-    s.style.visibility = (s.style.visibility === 'visible') ? 'hidden' : 'visible';
+    function blink() 
+    {
+        var blinks = document.getElementsByTagName( "blink" );
+        for ( var i = blinks.length - 1; i >= 0; i-- ) 
+        {
+            var s = blinks[ i ];
+            
+            if( currentlyTyping )   //this will make the keep continue stop blinking while typing out lines
+            {   
+                s.style.visibility = "hidden";
+            }
+            
+            else if( count >= lines.length )    //we are done with printing out so we can stop showing(and blinking) click to continue
+            {
+                return;
+            }
+            
+            else if( count > 0 )    //the > 0 is to stop from having the short flash of click to continue when the page loads
+            {
+                s.style.visibility = ( s.style.visibility === "visible" ) ? "hidden" : "visible";
+            }           
+        }
+        
+        window.setTimeout(blink, 350);
     }
-    window.setTimeout(blink, 350);
+
+    if ( document.addEventListener ) 
+    {
+        document.addEventListener( "DOMContentLoaded", blink, false );
     }
-    if (document.addEventListener) document.addEventListener("DOMContentLoaded", blink, false);
-    else if (window.addEventListener) window.addEventListener("load", blink, false);
-    else if (window.attachEvent) window.attachEvent("onload", blink);
-    else window.onload = blink;
+    
+    else if ( window.addEventListener ) 
+    {
+        window.addEventListener( "load", blink, false );
+    }
+    
+    else if ( window.attachEvent ) 
+    {
+        window.attachEvent( "onload", blink );
+    }
+    
+    else 
+    {
+        window.onload = blink;
+    }
 </script>
     
 <?php
