9f3c22609ed9f1c6ff8a8f3e7e5b3a185427816c
jtf28 <jtf28@pitt.edu>
2016-03-12 17:28:01 -0500

    all bugs in the php code for the portfolio are now fixed

--- portfolio.php
+++ portfolio.php
@@ -73,25 +73,30 @@
         die( "Error connecting to database" );
     }
         
-    if( isset( $_GET[ "name" ] ) )  //sort by name and set the variables for later
+    /*For part two of the project I may add a sort type or two such as what the project was done for: school, myself, research etc and also languages used,
+    if I do that I will probably refactor the portfolio to object oriented code since it will probably get more confusing then it is now, but it does work properly now */
+    if( isset( $_GET[ "name" ] ) )  //sort by NAME and set the variables for sorting buttons
     {
         $name =  $_GET[ "name" ];
         $result = getQuery( $portfolioDB, $name, "Name" );
         $currComparator = "z";
         $sortType = "NAME";
+        $nextName = getNextHref( $name );
+        $time = "fa-sort";
+        $nextTime = "fa-sort-asc";
     }
     
-    else if( isset( $_GET[ "time" ] ) ) //sort by time and set the variables for later
+    else if( isset( $_GET[ "time" ] ) ) //sort by TIME and set the variables for sorting buttons
     {
         $time = $_GET[ "time" ];
         $result = getQuery( $portfolioDB, $time, "Month Finished" );
         $currComparator = "0";
         $sortType = "YEAR";
+        $name = "fa-sort";
+        $nextName = "fa-sort-asc";
+        $nextTime = getNextHref( $time );
     }
-    
-    $nextName = getNextHref( $name );
-    $nextTime = getNextHref( $time );
-        
+
     //print out sorting buttons
     echo "
     <ul class=\"nav nav-pills nav-justified\">
@@ -99,6 +104,7 @@
         <li><a href=\"portfolio.php?time={$nextTime}\">Time Finished <i class=\"fa {$time}\"></i></a></li>
     </ul>";
     
+    //print out each project and its description
     echo "<hr class=\"brown\">";
     while( $row = $result->fetch_assoc() )
     {
