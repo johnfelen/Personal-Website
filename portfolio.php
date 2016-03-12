4222a48d74d509e4a93b7ea833c3ec937b9f0418
jtf28 <jtf28@pitt.edu>
2016-03-12 16:51:31 -0500

    moved more of the portfolio.php to functions and and it prints out character and time for when sorting but it has a bug of printing out everything

--- portfolio.php
+++ portfolio.php
@@ -1,4 +1,12 @@
 <?php
+    echo $_GET[ "name" ];
+    echo $_GET[ "time" ];
+
+    if( !isset( $_GET[ "name" ] ) && !isset( $_GET[ "time" ] ) )    //redirect if they are going to portfolio wil nothing in get, so it will sort by name of project by default
+    {
+        header( "Location: portfolio.php?name=fa-sort-asc");
+    }
+    
     function getNextHref( $currHref )
     {
         if( $currHref === "fa-sort" )
@@ -10,18 +18,50 @@
         {
             return "fa-sort-desc";
         }
+        
+        else
+        {
+            return "fa-sort-asc";
+        }
     }
     
     function getNextQuery( $portfolioDB, $currSortChoice, $orderAttribute )
     {
         if( $currSortChoice === "fa-sort-asc" )
         {
-            return $portfolioDB->query( "SELECT * FROM `project descriptions` ORDER BY `{$orderAttribute}` ASC" );
+            $result = $portfolioDB->query( "SELECT * FROM `project descriptions` ORDER BY `{$orderAttribute}` ASC" );
         }
         
         else if( $currSortChoice === "fa-sort-desc" )
         {
-            return $portfolioDB->query( "SELECT * FROM `project descriptions` ORDER BY `{$orderAttribute}` DESC" );
+            $result = $portfolioDB->query( "SELECT * FROM `project descriptions` ORDER BY `{$orderAttribute}` DESC" );
+        }
+        
+        if( !$result )
+        {
+            die( "Error with query" );
+        }
+        
+        else
+        {
+            return $result;
+        }
+    }
+    
+    function printNewSort( $currComparator, $sortType, $row ) //checks if the next item would be in a different "tier" and prints it out if it is, for example if the year changes print out the year change
+    {
+        if( $sortType === "NAME" && $currComparator !== $row[ "Name" ][ 0 ] )
+        {
+            $currComparator = $row[ "Name" ][ 0 ];
+            echo "<p class=\"font-ubuntu-mono font-header font-center brown\">{$currComparator}</p>";
+            return $currComparator;
+        }
+        
+        else if( $sortType === "YEAR" && $currComparator !== date( "Y", strtotime( $row[ "Month Finished" ] ) ) )
+        {
+            $currComparator = date( "Y", strtotime( $row[ "Month Finished" ] ) );
+            echo "<p class=\"font-ubuntu-mono font-header font-center brown\">{$currComparator}</p>";
+            return $currComparator;
         }
     }
     
@@ -35,53 +75,39 @@
     {
         die( "Error connecting to database" );
     }
-    
-    echo $_GET[ "name" ];
-    
-    if( isset( $_GET[ "name" ] ) )
+        
+    if( isset( $_GET[ "name" ] ) )  //sort by name and set the variables for later
     {
-        $result = getNextQuery( $portfolioDB, $_GET[ "name" ], "name" );
+        $name =  $_GET[ "name" ];
+        $result = getNextQuery( $portfolioDB, $name, "name" );
+        $currComparator = "z";
+        $sortType = "NAME";
     }
     
-    else if( isset( $_GET[ "time" ] ) )
+    else if( isset( $_GET[ "time" ] ) ) //sort by time and set the variables for later
     {
-        $result = getNextQuery( $portfolioDB, $_GET[ "time" ], "time" );
-    }
-    
-    else    //default start
-    {
-        $result = "HELLO";
-        $name = "fa-sort";
-        $time = "fa-sort";
-    }
-    
-    if( !$result )
-    {
-        die( "Error with query" );
+        $time = $_GET[ "time" ];
+        $result = getNextQuery( $portfolioDB, $time, "time" );
+        $currComparator = "0";
+        $sortType = "YEAR";
     }
     
     $nextName = getNextHref( $name );
-    $nextTime = getNextHref( $name );
+    $nextTime = getNextHref( $time );
         
     //print out sorting buttons
     echo "
     <ul class=\"nav nav-pills nav-justified\">
         <li><a href=\"portfolio.php?name={$nextName}\">Name of Project <i class=\"fa {$name}\"></i></a></li>
-        <li><a href=\"portfolio.php?time={$nextTime}\">Name of Project <i class=\"fa {$time}\"></i></a></li>
+        <li><a href=\"portfolio.php?time={$nextTime}\">Time Finished <i class=\"fa {$time}\"></i></a></li>
     </ul>";
     
-    //<!--fa-sort-desc   fa-sort-asc-->
-
     //formatting and printing out project names(that are linked) and printing their description
-    $currYear = "0";
+
     echo "<hr class=\"brown\">";
     while( $row = $result->fetch_assoc() )
     {
-        if( $currYear != date( "Y", strtotime( $row[ "Month Finished" ] ) ) )
-        {
-            $currYear = date( "Y", strtotime( $row[ "Month Finished" ] ) );
-            echo "<p class=\"font-ubuntu-mono font-header font-center brown\">{$currYear}</p>";
-        }
+        printNewSort( $currComparator, $sortType, $row );
         echo "
         <hr class=\"brown\">
         <div class=\"row vertical-center\">	
