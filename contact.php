2d6081bc2c10cacb9abb831075dd9ed83724f840
jtf28 <jtf28@pitt.edu>
2016-03-12 19:10:14 -0500

    hopefully this fixes everything

--- contact.php
+++ contact.php
@@ -5,11 +5,7 @@
     include( "php_include_files/start-row-10.php" );
 ?>
 
-<<<<<<< HEAD
 <p class="font-vollkorn font-small font-center brown" >
-=======
-<p class="font-vollkorn font-medium font-center brown" >
->>>>>>> 6df4286fa3fb728fb522623c6309846a7a78cf01
     I was born at a young age of zero months in Pittsburgh, Pennsylvania. &nbsp;
     I am now 247 months old. &nbsp;
     In my trek for an education I went to the school district known as Mt. Lebanon. &nbsp;
@@ -27,11 +23,7 @@
 </p>
 
 <br>
-<<<<<<< HEAD
 <p class="font-vollkorn font-small font-center brown" >
-=======
-<p class="font-vollkorn font-medium font-center brown" >
->>>>>>> 6df4286fa3fb728fb522623c6309846a7a78cf01
     If you wish to contact me, please do so through my e-mail address: johnfelen@pitt.edu.  &nbsp;
     A link to my r&eacute;sum&eacute; is in the footer. &nbsp;
 </p>
