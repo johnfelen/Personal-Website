9ad9771063991e5800ceb501fc4526cb90e320b8
jtf28 <jtf28@pitt.edu>
2016-04-17 19:26:36 -0400

    updated resume and changed some links for files

--- index.php
+++ index.php
@@ -4,13 +4,13 @@
     {
         header( "Location: static-index.php" );
     }
-    
+
     else if( isset( $_SESSION[ "lastStartTime" ] ) )    //its been 15 minutes since they first came here so show them this page and delete the old session
     {
         session_unset();
         session_destroy();
     }
-    
+
     $_SESSION[ "lastStartTime" ] = time();  //fall through and set the time variable, the 15 minutes is so that the user won't have to continually go through the home page print outs wihtin a normal viewing of my website
     //which I deem to be 15 minutes, and if they want to see it again, 15 minutes is a short amount of time to do something else and come back
     $pageName = "Home";
@@ -18,12 +18,12 @@
     include( "php_include_files/header.php" );
     include( "php_include_files/start-row-10.php" );
 ?>
-    
+
 <!--Pokemon reference for a little humour-->
 <div class="row">
     <div class="col-xs-3"></div>
         <div class="col-xs-6">
-            <img src="../images/me.jpg" class="picture" alt="Picture of Me" width="100%"/>
+            <img src="./images/me.jpg" class="picture" alt="Picture of Me" width="100%"/>
             <blink class="invisible">
                 <p class="font-vollkorn font-medium font-center brown" id="broken"></p>
                 <p class="font-ubuntu-mono font-medium font-center brown" id="continue">
@@ -41,61 +41,61 @@
 <!--based on second last response on http://stackoverflow.com/questions/18105152/alternative-for-blink since the actual <blink> tag is deprecated, I wonder why
 NOTE: I also tried to move the javascript into the home.js file but it did not work and kept saying that blink() was not defined, since it was not an error I decided to work on other parts of the project with my time-->
 <script type="text/javascript">
-    function blink() 
+    function blink()
     {
         var blinks = document.getElementsByTagName( "blink" );
-        for ( var i = blinks.length - 1; i >= 0; i-- ) 
+        for ( var i = blinks.length - 1; i >= 0; i-- )
         {
             var s = blinks[ i ];
-            
+
             if( currentlyTyping )   //this will make the keep continue stop blinking while typing out lines
-            {   
+            {
                 s.style.visibility = "hidden";
             }
-            
+
             else if( count == lines.length + 1 )    //they now can "choose" their name so change "click to continue" to "choose your name", because of the below else if must have the s.style.visibility line
             {
                 document.getElementById( "continue" ).innerHTML =  "Choose Your Name<br>";
                 s.style.visibility = ( s.style.visibility === "visible" ) ? "hidden" : "visible";
             }
-            
+
             else if( count == lines.length + 2 )    //we are done with blinking, make the text visible so the "loading" simulation will show
             {
                 s.style.visibility = "visible";
                 return;
             }
-            
+
             else if( count > 0 )    //the > 0 is to stop from having the short flash of click to continue when the page loads
             {
                 s.style.visibility = ( s.style.visibility === "visible" ) ? "hidden" : "visible";
-            }           
+            }
         }
-        
+
         window.setTimeout(blink, 350);
     }
 
     //these for are from the stack overflow, it works so I'm not going to change it
-    if ( document.addEventListener ) 
+    if ( document.addEventListener )
     {
         document.addEventListener( "DOMContentLoaded", blink, false );
     }
-    
-    else if ( window.addEventListener ) 
+
+    else if ( window.addEventListener )
     {
         window.addEventListener( "load", blink, false );
     }
-    
-    else if ( window.attachEvent ) 
+
+    else if ( window.attachEvent )
     {
         window.attachEvent( "onload", blink );
     }
-    
-    else 
+
+    else
     {
         window.onload = blink;
     }
 </script>
-    
+
 <?php
     include( "php_include_files/end-row-10.php" );
     include( "php_include_files/footer.php" );
--- static-index.php
+++ static-index.php
@@ -1,26 +1,26 @@
 <?php
-    session_start();    
+    session_start();
     if( !isset( $_SESSION[ "lastStartTime" ] ) || ( isset( $_SESSION[ "lastStartTime" ] ) && time() - $_SESSION[ "lastStartTime" ] > 900 ) )  //its been 15 minutes so just send them back to the non-static home page
     {
         header( "Location: index.php" );
     }
-    
+
     $pageName = "Home";
     $glyphiconName = "home";
     include( "php_include_files/header.php" );
     include( "php_include_files/start-row-10.php" );
 ?>
-    
+
 <!--Pokemon reference for a little humour-->
 <div class="row">
     <div class="col-xs-3"></div>
         <div class="col-xs-6">
-            <img src="../images/me.jpg" class="picture" alt="Picture of Me" width="100%"/>
+            <img src="./images/me.jpg" class="picture" alt="Picture of Me" width="100%"/>
             <blink>
                 <p class="font-vollkorn font-medium font-center brown" id="broken">
                     It looks like the game froze.&nbsp;
                     Maybe try to blow into the cartridge and try again in 15 minutes. &nbsp;
-                    While you are here, feel free to explore my personal website. 
+                    While you are here, feel free to explore my personal website.
                 </p>
                 <p class="font-ubuntu-mono font-medium font-center brown" id="continue">
                     <i class="fa fa-spinner fa-3x brown"></i>
@@ -52,7 +52,7 @@
     </div>
     <div class="col-xs-3"></div>
 </div>
-    
+
 <?php
     include( "php_include_files/end-row-10.php" );
     include( "php_include_files/footer.php" );
