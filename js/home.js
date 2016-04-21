cc5bcb640432b382e0ffc0d647a7140cf90f138a
jtf28 <jtf28@pitt.edu>
2016-04-21 10:01:35 -0400

    did some more refactoring in home.js(made the initial call to blink only one line) and fixed a bug in the static-index.php where it would start printing as if it was index.php

--- js/home.js
+++ js/home.js
@@ -131,33 +131,15 @@ function toggleVisibility() //wrapper class for visibility, since jquery toggle 
     }
 }
 
-//these for are from the stack overflow, it works so I'm not going to change it
-if ( document.addEventListener )
-{
-    document.addEventListener( "DOMContentLoaded", blink, false );
-}
-
-else if ( window.addEventListener )
-{
-    window.addEventListener( "load", blink, false );
-}
-
-else if ( window.attachEvent )
-{
-    window.attachEvent( "onload", blink );
-}
-
-else
-{
-    window.onload = blink;
-}
-
 $( document ).ready( function()
 {
     printNextLine();
+    blink();
 });
 
 $( "#main-container" ).click( function()
 {
     printNextLine();
 });
+// http://stackoverflow.com/questions/8823925/how-to-return-an-array-from-an-ajax-call
+// http://stackoverflow.com/questions/2410773/how-to-return-data-from-php-to-a-jquery-ajax-call
--- static-index.php
+++ static-index.php
@@ -16,19 +16,19 @@
     <div class="col-xs-3"></div>
         <div class="col-xs-6">
             <img src="images/me.jpg" class="picture" alt="Picture of Me" width="100%"/>
-                <p class="font-main font-medium font-center color" id="broken">
+                <p class="font-main font-medium font-center color" id="static-broken">
                     It looks like the game froze.&nbsp;
                     Maybe try to blow into the cartridge and try again in 15 minutes. &nbsp;
                     While you are here, feel free to explore my personal website.
                 </p>
-                <p class="font-ubuntu-mono font-medium font-center color" id="continue">
+                <p class="font-ubuntu-mono font-medium font-center color" id="static-continue">
                     <i class="fa fa-spinner fa-3x color"></i>
                 </p>
         </div>
     <div class="col-xs-3"></div>
 </div>
 
-<p class="font-ubuntu-mono font-medium font-center color" id="pokemon">
+<p class="font-ubuntu-mono font-medium font-center color" id="static-pokemon">
     Hello there! Welcome to the world of Earth! <br>
     My name is John Felen! People call me John! <br>
     This world is inhabited by creatures called humans! <br>
@@ -38,10 +38,10 @@
     First, what is your name? <br>
 </p>
 
-<div class="row" id="names">
+<div class="row" id="static-names">
     <div class="col-xs-3"></div>
     <div class="col-xs-6 container-main">
-        <table class="font-ubuntu-mono font-header color color-border font-center fill-n-center" id="choose-name">
+        <table class="font-ubuntu-mono font-header color color-border font-center fill-n-center" id="static-choose-name">
             <tr><td>NEW NAME</td></tr>
             <tr><td>RED</td></tr>
             <tr><td>ASH</td></tr>
