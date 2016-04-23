cc973043918ab51b8fd554b562940c9aecf36493
jtf28 <jtf28@pitt.edu>
2016-04-23 01:11:34 -0400

    made the code for portoflio into a class, although it works it needs a lot of refactoring

--- portfolio.php
+++ portfolio.php
@@ -9,35 +9,7 @@
     include( "./format_files/header.php" );
     require( "./server_functionality/portfolio-functions.php" );
 
-    $portfolioDB = new mysqli( "localhost", "root", "jfelen62", "personal website" );
-    if( $portfolioDB->connect_error )
-    {
-        die( "Error connecting to database" );
-    }
-
-    /*For part two of the project I may add a sort type or two such as what the project was done for: school, myself, research etc and also languages used,
-    if I do that I will probably refactor the portfolio to object oriented code since it will probably get more confusing then it is now, but it does work properly now */
-    if( isset( $_GET[ "name" ] ) )  //sort by NAME and set the variables for sorting buttons
-    {
-        $name =  $_GET[ "name" ];
-        $result = queryProjects( $portfolioDB, $name, "Name" );
-        $currComparator = "z";
-        $sortType = "NAME";
-        $nextName = getNextHref( $name );
-        $time = "fa-sort";
-        $nextTime = "fa-sort-asc";
-    }
-
-    else if( isset( $_GET[ "time" ] ) ) //sort by TIME and set the variables for sorting buttons
-    {
-        $time = $_GET[ "time" ];
-        $result = queryProjects( $portfolioDB, $time, "Month Finished" );
-        $currComparator = "0";
-        $sortType = "YEAR";
-        $name = "fa-sort";
-        $nextName = "fa-sort-asc";
-        $nextTime = getNextHref( $time );
-    }
+    $portfolioData = new PortfolioSort();
 
     //print out sorting buttons
     echo "
@@ -46,28 +18,7 @@
         <li class=\"port-padding\"><a href=\"portfolio.php?time={$nextTime}\">Time Finished <i class=\"fa {$time}\"></i></a></li>
     </ul>";
 
-    //print out each project and its description
-    echo "<hr class=\"color-border\">";
-    while( $row = $result->fetch_assoc() )
-    {
-        $currComparator = printTierChange( $currComparator, $sortType, $row );
-        echo "
-        <hr class=\"color-border\">
-        <div class=\"row vertical-center\">
-            <div class=\"col-xs-3\">
-                <p class=\"font-title font-large font-center colored-link rotate\">
-                    <a href=\"{$row[ "Link" ]}\" title=\"{$row[ "Link" ]}\" target=\"_blank\">{$row[ "Name" ]}</a>
-                </p>
-            </div>
+    echo $portfolioData->getSortedProjects();
 
-            <div class=\"col-xs-9\">
-                <p class=\"font-main font-center font-small color\">{$row[ "Description" ]}</p>
-            </div>
-        </div>
-        <hr class=\"color-border\">";
-    }
-    echo "<hr class=\"color-border\">";
-
-    mysqli_close( $portfolioDB );
     include( "./format_files/footer.php" );
 ?>
--- server_functionality/portfolio-functions.php
+++ server_functionality/portfolio-functions.php
@@ -1,59 +1,133 @@
 <?php
-    function getNextHref( $currHref )   //will figure out which attribute will be passed through $_GET[] for each sort button
+    class PortfolioSort
     {
-        if( $currHref === "fa-sort" )
+        function __construct()
         {
-            return "fa-sort-asc";
+            $this->portfolioDB = new mysqli( "localhost", "root", "jfelen62", "personal website" );
+
+            if( $this->portfolioDB->connect_error )
+            {
+                die( "Error connecting to database" );
+            }
+
+            if( isset( $_GET[ "name" ] ) )  //sort by NAME and set the variables for sorting buttons
+            {
+                $name = $_GET[ "name" ];
+                $this->result = $this->queryProjects( $name, "Name" );
+                $this->currComparator = "z";
+                $this->sortType = "NAME";
+                $nextName = $this->getNextHref( $name );
+                $time = "fa-sort";
+                $nextTime = "fa-sort-asc";
+            }
+
+            else if( isset( $_GET[ "time" ] ) ) //sort by TIME and set the variables for sorting buttons
+            {
+                $time = $_GET[ "time" ];
+                $this->result = $this->queryProjects( $time, "Month Finished" );
+                $this->currComparator = "0";
+                $this->sortType = "YEAR";
+                $name = "fa-sort";
+                $nextName = "fa-sort-asc";
+                $nextTime = $this->getNextHref( $time );
+            }
         }
 
-        else if( $currHref === "fa-sort-asc" )
+        function __destruct()
         {
-            return "fa-sort-desc";
+            mysqli_close( $this->portfolioDB );
         }
 
-        else
+        public function setFunction( $compare )
         {
-            return "fa-sort-asc";
-        }
-    }
-
-    function queryProjects( $portfolioDB, $currSortChoice, $orderAttribute ) //query the order the projects will be displayed
-    {
-        if( $currSortChoice === "fa-sort-asc" )
-        {
-            $result = $portfolioDB->query( "SELECT * FROM `project descriptions` ORDER BY `{$orderAttribute}` ASC" );
+            $this->currComparator = $compare;
         }
 
-        else if( $currSortChoice === "fa-sort-desc" )
+        public function getSortedProjects() //return all the projects and their descriptions formatted for the webpage
         {
-            $result = $portfolioDB->query( "SELECT * FROM `project descriptions` ORDER BY `{$orderAttribute}` DESC" );
+            $output = "<hr class=\"color-border\">";
+            while( $row = $this->result->fetch_assoc() )
+            {
+                $output .= $this->getTierChange( $row );
+                $output .= "
+                <hr class=\"color-border\">
+                <div class=\"row vertical-center\">
+                    <div class=\"col-xs-3\">
+                        <p class=\"font-title font-large font-center colored-link rotate\">
+                            <a href=\"{$row[ "Link" ]}\" title=\"{$row[ "Link" ]}\" target=\"_blank\">{$row[ "Name" ]}</a>
+                        </p>
+                    </div>
+
+                    <div class=\"col-xs-9\">
+                        <p class=\"font-main font-center font-small color\">{$row[ "Description" ]}</p>
+                    </div>
+                </div>
+                <hr class=\"color-border\">";
+            }
+            $output .= "<hr class=\"color-border\">";
+
+            return $output;
         }
 
-        if( !$result )
+        private function getTierChange( $row ) //checks if the next item would be in a different "tier" and prints it out if it is, such as when the year changes or the starting letter of the project changes print out the change
         {
-            die( "Error with query" );
+            if( $this->sortType === "NAME" && $this->currComparator !== $row[ "Name" ][ 0 ] )
+            {
+                $this->currComparator = $row[ "Name" ][ 0 ];
+                return "<p class=\"font-title font-header font-center color\">{$this->currComparator}</p>";
+            }
+
+            else if( $this->sortType === "YEAR" && $this->currComparator !== date( "Y", strtotime( $row[ "Month Finished" ] ) ) )
+            {
+                $this->currComparator = date( "Y", strtotime( $row[ "Month Finished" ] ) );
+                return "<p class=\"font-title font-header font-center color\">{$this->currComparator}</p>";
+            }
+
+            else
+            {
+                return "";
+            }
         }
 
-        else
+        private function getNextHref( $currHref )   //will figure out which attribute will be passed through $_GET[] for each sort button
         {
-            return $result;
-        }
-    }
+            if( $currHref === "fa-sort" )
+            {
+                return "fa-sort-asc";
+            }
 
-    function printTierChange( $currComparator, $sortType, $row ) //checks if the next item would be in a different "tier" and prints it out if it is, such as when the year changes or the starting letter of the project changes print out the change
-    {
-        if( $sortType === "NAME" && $currComparator !== $row[ "Name" ][ 0 ] )
-        {
-            $currComparator = $row[ "Name" ][ 0 ];
-            echo "<p class=\"font-title font-header font-center color\">{$currComparator}</p>";
-        }
+            else if( $currHref === "fa-sort-asc" )
+            {
+                return "fa-sort-desc";
+            }
 
-        else if( $sortType === "YEAR" && $currComparator !== date( "Y", strtotime( $row[ "Month Finished" ] ) ) )
-        {
-            $currComparator = date( "Y", strtotime( $row[ "Month Finished" ] ) );
-            echo "<p class=\"font-title font-header font-center color\">{$currComparator}</p>";
+            else
+            {
+                return "fa-sort-asc";
+            }
         }
 
-        return $currComparator;
+        private function queryProjects( $currSortChoice, $orderAttribute )  //query the order the projects will be displayed
+        {
+            if( $currSortChoice === "fa-sort-asc" )
+            {
+                $result = $this->portfolioDB->query( "SELECT * FROM `project descriptions` ORDER BY `{$orderAttribute}` ASC" );
+            }
+
+            else if( $currSortChoice === "fa-sort-desc" )
+            {
+                $result = $this->portfolioDB->query( "SELECT * FROM `project descriptions` ORDER BY `{$orderAttribute}` DESC" );
+            }
+
+            if( !$result )
+            {
+                die( "Error with query" );
+            }
+
+            else
+            {
+                return $result;
+            }
+        }
     }
 ?>
