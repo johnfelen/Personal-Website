aab14190bbd4bb224f5845a2bb738dbc310b95f3
jtf28 <jtf28@pitt.edu>
2016-04-24 23:13:50 -0400

    added an htaccess to redirect to a naked website( since the cookies don't save between www and naked and their are, for some reason, some bugs on the www part that are not on the naked part ) and updated the answers.txt in legacy

--- format_files/footer.php
+++ format_files/footer.php
@@ -1,29 +1,29 @@
-    <!--close of the main container-->
-    </div>
-    <?php include( "./format_files/end-row-10.php" ); ?>
+        <!--close of the main container-->
+        </div>
+        <?php include( "./format_files/end-row-10.php" ); ?>
 
-    <!--start of the actual footer part-->
-    <?php include( "./format_files/start-row-10.php" ); ?>
-    <hr class="color-border">
-    <div class="vertical-center font-main font-small">
-        <p class="footer-left color" id="start-tour">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;John Felen's Personal Website</p>
-        <p class="footer-right colored-link" id="footer-links">
-            <a href="other_files/JohnFelenRésumé.pdf" title="Download my R&eacute;sum&eacute;" target="_blank"><i class="fa fa-file-text fa-2x"></i></a>
-            &nbsp;&nbsp;&nbsp;
-            <a href="https://github.com/johnfelen" title="Check out my Github" target="_blank"><i class="fa fa-github fa-2x"></i></a>
-            &nbsp;&nbsp;&nbsp;
-            <a href="https://www.linkedin.com/in/johnfelen" title="Connect with Me" target="_blank"><i class="fa fa-linkedin fa-2x"></i></a>
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        </p>
-    </div>
-    <hr class="color-border footer-break">
-    <?php include( "./format_files/end-row-10.php" ); ?>
+        <!--start of the actual footer part-->
+        <?php include( "./format_files/start-row-10.php" ); ?>
+        <hr class="color-border">
+        <div class="vertical-center font-main font-small">
+            <p class="footer-left color" id="start-tour">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;John Felen's Personal Website</p>
+            <p class="footer-right colored-link" id="footer-links">
+                <a href="other_files/JohnFelenRésumé.pdf" title="Download my R&eacute;sum&eacute;" target="_blank"><i class="fa fa-file-text fa-2x"></i></a>
+                &nbsp;&nbsp;&nbsp;
+                <a href="https://github.com/johnfelen" title="Check out my Github" target="_blank"><i class="fa fa-github fa-2x"></i></a>
+                &nbsp;&nbsp;&nbsp;
+                <a href="https://www.linkedin.com/in/johnfelen" title="Connect with Me" target="_blank"><i class="fa fa-linkedin fa-2x"></i></a>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            </p>
+        </div>
+        <hr class="color-border footer-break">
+        <?php include( "./format_files/end-row-10.php" ); ?>
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-    <script src="http://codegena.com/assets/js/image-preview-for-link.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-tour/0.10.3/js/bootstrap-tour.min.js"></script>
-    <script src="js/personal-website.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+        <script src="http://codegena.com/assets/js/image-preview-for-link.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-tour/0.10.3/js/bootstrap-tour.min.js"></script>
+        <script src="js/personal-website.min.js"></script>
     </body>
 </html>
