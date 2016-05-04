0445feaf50f22178908371d29a9727cfbb68326f
jtf28 <jtf28@pitt.edu>
2016-05-04 02:04:37 -0400

    updated the styling of the footer so with the tour the backdrop highlight is pretty even now

--- format_files/footer.php
+++ format_files/footer.php
@@ -5,18 +5,21 @@
         <!--start of the actual footer part-->
         <?php include( "./format_files/start-row-10.php" ); ?>
         <hr class="color-border">
-        <div class="vertical-center font-main font-small">
-            <p class="footer-left color" id="start-tour">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;John Felen's Personal Website</p>
-            <p class="footer-right colored-link" id="footer-links">
-                <a href="other_files/JohnFelenRésumé.pdf" title="Download my R&eacute;sum&eacute;" target="_blank"><i class="fa fa-file-text fa-2x"></i></a>
-                &nbsp;&nbsp;&nbsp;
-                <a href="https://github.com/johnfelen" title="Check out my Github" target="_blank"><i class="fa fa-github fa-2x"></i></a>
-                &nbsp;&nbsp;&nbsp;
-                <a href="https://www.linkedin.com/in/johnfelen" title="Connect with Me" target="_blank"><i class="fa fa-linkedin fa-2x"></i></a>
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            </p>
+        <div class="row vertical-center font-main font-footer">
+            <div class="col-xs-8 colored-link">
+                <a href="#" title="Start Tour"><span class="footer-padding-left" id="start-tour">John Felen's Personal Website</span></a>
+            </div>
+            <div class="col-xs-4 footer-right">
+                <span class="colored-link footer-padding-right" id="footer-links">
+                    <a href="other_files/JohnFelenRésumé.pdf" title="Download my R&eacute;sum&eacute;" target="_blank"><i class="fa fa-file-text fa-lg"></i></a>
+                    &nbsp;&nbsp;&nbsp;
+                    <a href="https://github.com/johnfelen" title="Check out my Github" target="_blank"><i class="fa fa-github fa-lg"></i></a>
+                    &nbsp;&nbsp;&nbsp;
+                    <a href="https://www.linkedin.com/in/johnfelen" title="Connect with Me" target="_blank"><i class="fa fa-linkedin fa-lg"></i></a>
+                </span>
+            </div>
         </div>
-        <hr class="color-border footer-break">
+        <hr class="color-border">
         <?php include( "./format_files/end-row-10.php" ); ?>
 
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
