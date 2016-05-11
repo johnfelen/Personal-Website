5a639b2f01621da41589ae32088260797cc8e708
jtf28 <jtf28@pitt.edu>
2016-05-10 23:19:41 -0400

    made a basic, somewhat broken Gruntfile and added the dependencies

--- src/format_files/footer.php
+++ src/format_files/footer.php
@@ -28,14 +28,16 @@
             <?php include( "./format_files/end-row-10.php" ); ?>
         </div>
 
-        
+
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
         <script src="http://codegena.com/assets/js/image-preview-for-link.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-tour/0.10.3/js/bootstrap-tour.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/1000hz-bootstrap-validator/0.10.2/validator.min.js"></script>
         <script src="https://cdn.jsdelivr.net/bootstrap.autohidingnavbar/1.0.5/jquery.bootstrap-autohidingnavbar.min.js"></script>
+        <!-- build:js inline -->
         <script src="js/personal-website.min.js"></script>
+        <!-- /build -->
     </body>
 </html>
 
